7ac8aec8d4bb794cad4d2c5d5913250157686c94
laczor <laczor@gmail.com>
2017-08-24 19:22:50 +0200

    Update App.js

--- Lecture 14 PokeDashboard Making API Calls/src/App.js
+++ Lecture 14 PokeDashboard Making API Calls/src/App.js
@@ -3,18 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import 'whatwg-fetch';
 
+//** written in ES6
 class App extends Component {
 
   constructor(props) {
     super(props);
-
+//array of pokemon state
     this.state = {
       pokemon: []
     };
-
+//binding the function to this component
     this.loadPokemon = this.loadPokemon.bind(this);
   }
-
+//makes a call to an API, which response will return a jsoon file, then this json will be moved
+//if there is an error, it will be catched in the fetch, then all of the api call will be fetched in and printed out in console
   loadPokemon(url) {
     fetch(url)
       .then(response => {
@@ -29,11 +31,12 @@ class App extends Component {
           count: json.count
         });
         console.log(this.state)
+      
       }).catch(err => {
         console.log(err)
       })
   }
-
+//*** once the component will mount, it should call the loadpokemon function, parsing the API call
   componentWillMount() {
     this.loadPokemon(`${this.props.baseUrl}/pokemon/`);
   }
